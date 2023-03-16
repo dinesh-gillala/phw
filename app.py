0d13dd54dc1c356bb291241942b00a456bdac0d5
dinesh-gillala <dgillala@zscaler.com>
2023-03-16 13:25:59 +0530

    push code

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.run(port=8080, host="0.0.0.0")
